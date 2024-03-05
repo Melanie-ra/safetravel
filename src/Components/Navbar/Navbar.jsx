abcfa6712bc26b43727a1211192d447e2d84f7e1
JorgeL99 <sairachambi@gmail.com>
2024-03-05 08:41:25 -0500

    av2

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,19 +1,68 @@
 import React from 'react'
 import './navbar.css'
+import { SiYourtraveldottv } from "react-icons/si";
+import { AiFillCloseCircle } from "react-icons/ai";
+import { TbGridDots } from "react-icons/tb";
 
 const Navbar = () => {
   return (
     <section className='navBarSection'>
       <div className="header">
+        
         <div className="logoDiv">
-          <a href="#" className="log">
-            <h1>
-              
+          <a href="#" className="logo">
+            <h1 className='flex'>
+            <SiYourtraveldottv className="icon"/>DOT
             </h1>
           </a>
         </div>
-      </div>
 
+        <div className="navBar">
+          <ul className='navLists flex'>
+
+            <li className="navItem">
+              <a href='#' className='navLink'>Home</a>
+            </li>
+
+            <li className="navItem">
+              <a href='#' className='navLink'>Products</a>
+            </li>
+
+            <li className="navItem">
+              <a href='#' className='navLink'>Resources</a>
+            </li>
+
+            <li className="navItem">
+              <a href='#' className='navLink'>Contacts</a>
+            </li>
+
+            <li className="navItem">
+              <a href='#' className='navLink'>Blog</a>
+            </li>
+
+            <div className="headerBtns flex">
+              <button className='btn loginBtn'>
+                <a href='#'>Login</a>
+              </button>
+
+              <button className='btn loginBtn'>
+                <a href='#'>Sign Up</a>
+              </button>
+
+            </div>
+          
+          </ul>
+
+          <div className="closeNavbar">
+          <AiFillCloseCircle className="icon" />
+          </div>
+
+        </div>
+
+        <div className="toggleNavbar">
+        <TbGridDots className="icon"/>
+        </div>      
+      </div>
     </section>
   )
 }
